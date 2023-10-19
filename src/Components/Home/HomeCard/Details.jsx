ea42ba428ebd076fe3a845ba15155737cb3b118f
shaoun11 <heyshaoun@gmail.com>
2023-10-20 01:51:34 +0600

    Details-page-dynamic

--- src/Components/Home/HomeCard/Details.jsx
+++ src/Components/Home/HomeCard/Details.jsx
@@ -8,12 +8,12 @@ const Details = () => {
     return (
         <div>
             <section  className="text-gray-700 body-font overflow-hidden bg-white">
-  <div  className="container px-5 py-24 mx-auto">
-    <div  className="lg:w-4/5 mx-auto flex flex-wrap">
-      <img alt="ecommerce"  className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={img}/>
+  <div  className=" lg:ml-12 lg:mr-12 md:ml-12 md:mr-12 py-24 mx-auto">
+    <div  className="lg:w-[100%] mx-auto flex flex-wrap">
+      <img alt="ecommerce"  className="lg:w-1/2 w-[800px] h-[800px] mt-8 object-cover object-center rounded border border-gray-200" src={img}/>
       <div  className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2  className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1  className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+        <h2  className="text-sm title-font font-semibold text-gray-500 tracking-widest">{BrandName}</h2>
+        <h1  className="text-gray-900 text-3xl title-font font-medium mb-1">{name}</h1>
         <div  className="flex mb-4">
           <span  className="flex items-center">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"  className="w-4 h-4 text-red-500" viewBox="0 0 24 24">
@@ -51,7 +51,7 @@ const Details = () => {
             </a>
           </span>
         </div>
-        <p  className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+        <p  className="leading-relaxed"> {description} </p>
         <div  className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
           <div  className="flex">
             <span  className="mr-3">Color</span>
@@ -77,8 +77,8 @@ const Details = () => {
           </div>
         </div>
         <div  className="flex">
-          <span  className="title-font font-medium text-2xl text-gray-900">$58.00</span>
-          <button  className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
+          <span  className="title-font font-medium text-2xl text-gray-900">{Price}</span>
+          <button  className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Add to Cart</button>
           <button  className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"  className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
